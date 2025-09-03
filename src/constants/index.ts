44dd0d5bde697683b59b8ce111dedbafcf44395d
mnats725 <56044398+mnats725@users.noreply.github.com>
2025-09-03 16:32:18 +0300

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const NEWS_API_BASE_URL = 'https://newsapi.org/v2';
-export const NEWS_API_KEY = '4a381b113f8845cbb28eb89e12e80d77';
+export const NEWS_API_KEY = '';
 export const PAGE_SIZE = 10;
 export const DEFAULT_COUNTRY = 'us';
 
